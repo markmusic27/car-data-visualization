cbae3796d9b945c3e21b9a8aca2fc2e5e1f5e9de
Mark Music <markmusic999@gmail.com>
2020-12-18 20:51:32 -0600

    feat(main): 🦣 Finsish graphing and data manipulation

---
+++ main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# LOAD DATASET / INIT SEABORN
+
+df = pd.read_csv("http://media.sundog-soft.com/SelfDriving/FuelEfficiency.csv")
+
+x = "# Gears"
+y = "CombMPG"
+
+sns.set()
+
+# SCATTER PLOT
+
+sns.scatterplot(x=x, y=y, data=df)
+
+# LINEAR REGRESSION PLOT
+
+sns.lmplot(x=x, y=y, data=df)
+
+
+# JOINT PLOT
+
+sns.jointplot(x=x, y=y, data=df)
+
+# BOX & WHISKER PLOT
+
+sns.set(rc={"figure.figsize": (5, 15)})
+
+ax = sns.boxplot(x=x, y=y, data=df)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+
+
+# SWARM PLOT
+
+sx = sns.swarmplot(x=x, y=y, data=df)
+sx.set_xticklabels(sx.get_xticklabels(), rotation=45)
+
+# CONJECTURE
